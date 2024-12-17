d77121c8a299d3c50791e107d90290fd7f882d00
PolyLiYJ <yjl.zju@gmail.com>
2024-12-17 21:01:10 +0800

    sdfsdaf

--- test.py
+++ test.py
@@ -30,7 +30,7 @@ from livelossplot import PlotLosses # pip install livelossplot
 
 import os
 
-os.environ["HF_HOME"] = "/root/autodl-tmp/cache/"
+# os.environ["HF_HOME"] = "/root/autodl-tmp/cache/"
 
 def generate(model, tokenizer, input_ids, assistant_role_slice, gen_config=None):
     if gen_config is None:
@@ -168,6 +168,7 @@ prompt = tokenizer.apply_chat_template(
     add_generation_prompt=True,
     tokenize = False
 )
+print(prompt)
 
 input_ids = tokenizer(prompt,return_tensors="pt")["input_ids"].to(model.device)
 
@@ -232,93 +233,93 @@ def load_template(file_path):
 # print("Generated Output:", output_text)
 
 
-# conv_template = load_conversation_template(template_name)
+conv_template = load_conversation_template(template_name)
 
-# suffix_manager = SuffixManager(tokenizer=tokenizer, 
-#               conv_template=conv_template, 
-#               instruction=user_prompt, 
-#               target=target, 
-#               adv_string=adv_string_init)
+suffix_manager = SuffixManager(tokenizer=tokenizer, 
+              conv_template=conv_template, 
+              instruction=user_prompt, 
+              target=target, 
+              adv_string=adv_string_init)
 
-# plotlosses = PlotLosses()
+plotlosses = PlotLosses()
 
-# not_allowed_tokens = None if allow_non_ascii else get_nonascii_toks(tokenizer) 
-# adv_suffix = adv_string_init
+not_allowed_tokens = None if allow_non_ascii else get_nonascii_toks(tokenizer) 
+adv_suffix = adv_string_init
 
-# for i in range(num_steps):
+for i in range(num_steps):
     
-#     # Step 1. Encode user prompt (behavior + adv suffix) as tokens and return token ids.
-#     input_ids = suffix_manager.get_input_ids(adv_string=adv_suffix)
-#     input_ids = input_ids.to(device)
+    # Step 1. Encode user prompt (behavior + adv suffix) as tokens and return token ids.
+    input_ids = suffix_manager.get_input_ids(adv_string=adv_suffix)
+    input_ids = input_ids.to(device)
     
-#     # Step 2. Compute Coordinate Gradient
-#     coordinate_grad = token_gradients(model, 
-#                     input_ids, 
-#                     suffix_manager._control_slice, 
-#                     suffix_manager._target_slice, 
-#                     suffix_manager._loss_slice)
+    # Step 2. Compute Coordinate Gradient
+    coordinate_grad = token_gradients(model, 
+                    input_ids, 
+                    suffix_manager._control_slice, 
+                    suffix_manager._target_slice, 
+                    suffix_manager._loss_slice)
     
-#     # Step 3. Sample a batch of new tokens based on the coordinate gradient.
-#     # Notice that we only need the one that minimizes the loss.
-#     with torch.no_grad():
+    # Step 3. Sample a batch of new tokens based on the coordinate gradient.
+    # Notice that we only need the one that minimizes the loss.
+    with torch.no_grad():
         
-#         # Step 3.1 Slice the input to locate the adversarial suffix.
-#         adv_suffix_tokens = input_ids[suffix_manager._control_slice].to(device)
+        # Step 3.1 Slice the input to locate the adversarial suffix.
+        adv_suffix_tokens = input_ids[suffix_manager._control_slice].to(device)
         
-#         # Step 3.2 Randomly sample a batch of replacements.
-#         new_adv_suffix_toks = sample_control(adv_suffix_tokens, 
-#                        coordinate_grad, 
-#                        batch_size, 
-#                        topk=topk, 
-#                        temp=1, 
-#                        not_allowed_tokens=not_allowed_tokens)
+        # Step 3.2 Randomly sample a batch of replacements.
+        new_adv_suffix_toks = sample_control(adv_suffix_tokens, 
+                       coordinate_grad, 
+                       batch_size, 
+                       topk=topk, 
+                       temp=1, 
+                       not_allowed_tokens=not_allowed_tokens)
         
-#         # Step 3.3 This step ensures all adversarial candidates have the same number of tokens. 
-#         # This step is necessary because tokenizers are not invertible
-#         # so Encode(Decode(tokens)) may produce a different tokenization.
-#         # We ensure the number of token remains to prevent the memory keeps growing and run into OOM.
-#         new_adv_suffix = get_filtered_cands(tokenizer, 
-#                                             new_adv_suffix_toks, 
-#                                             filter_cand=True, 
-#                                             curr_control=adv_suffix)
+        # Step 3.3 This step ensures all adversarial candidates have the same number of tokens. 
+        # This step is necessary because tokenizers are not invertible
+        # so Encode(Decode(tokens)) may produce a different tokenization.
+        # We ensure the number of token remains to prevent the memory keeps growing and run into OOM.
+        new_adv_suffix = get_filtered_cands(tokenizer, 
+                                            new_adv_suffix_toks, 
+                                            filter_cand=True, 
+                                            curr_control=adv_suffix)
         
-#         # Step 3.4 Compute loss on these candidates and take the argmin.
-#         logits, ids = get_logits(model=model, 
-#                                  tokenizer=tokenizer,
-#                                  input_ids=input_ids,
-#                                  control_slice=suffix_manager._control_slice, 
-#                                  test_controls=new_adv_suffix, 
-#                                  return_ids=True,
-#                                  batch_size=512) # decrease this number if you run into OOM.
+        # Step 3.4 Compute loss on these candidates and take the argmin.
+        logits, ids = get_logits(model=model, 
+                                 tokenizer=tokenizer,
+                                 input_ids=input_ids,
+                                 control_slice=suffix_manager._control_slice, 
+                                 test_controls=new_adv_suffix, 
+                                 return_ids=True,
+                                 batch_size=512) # decrease this number if you run into OOM.
 
-#         losses = target_loss(logits, ids, suffix_manager._target_slice)
+        losses = target_loss(logits, ids, suffix_manager._target_slice)
 
-#         best_new_adv_suffix_id = losses.argmin()
-#         best_new_adv_suffix = new_adv_suffix[best_new_adv_suffix_id]
+        best_new_adv_suffix_id = losses.argmin()
+        best_new_adv_suffix = new_adv_suffix[best_new_adv_suffix_id]
 
-#         current_loss = losses[best_new_adv_suffix_id]
+        current_loss = losses[best_new_adv_suffix_id]
 
-#         # Update the running adv_suffix with the best candidate
-#         adv_suffix = best_new_adv_suffix
-#         is_success = check_for_attack_success(model, 
-#                                  tokenizer,
-#                                  suffix_manager.get_input_ids(adv_string=adv_suffix).to(device), 
-#                                  suffix_manager._assistant_role_slice, 
-#                                  test_prefixes)
+        # Update the running adv_suffix with the best candidate
+        adv_suffix = best_new_adv_suffix
+        is_success = check_for_attack_success(model, 
+                                 tokenizer,
+                                 suffix_manager.get_input_ids(adv_string=adv_suffix).to(device), 
+                                 suffix_manager._assistant_role_slice, 
+                                 test_prefixes)
         
 
-#     # Create a dynamic plot for the loss.
-#     plotlosses.update({'Loss': current_loss.detach().cpu().numpy()})
-#     plotlosses.send() 
+    # Create a dynamic plot for the loss.
+    plotlosses.update({'Loss': current_loss.detach().cpu().numpy()})
+    plotlosses.send() 
     
-#     print(f"\nPassed:{is_success}\nCurrent Suffix:{best_new_adv_suffix}", end='\r')
+    print(f"\nPassed:{is_success}\nCurrent Suffix:{best_new_adv_suffix}", end='\r')
     
-#     # Notice that for the purpose of demo we stop immediately if we pass the checker but you are free to
-#     # comment this to keep the optimization running for longer (to get a lower loss). 
-#     if is_success:
-#         break
+    # Notice that for the purpose of demo we stop immediately if we pass the checker but you are free to
+    # comment this to keep the optimization running for longer (to get a lower loss). 
+    if is_success:
+        break
     
-#     # (Optional) Clean up the cache.
-#     del coordinate_grad, adv_suffix_tokens ; gc.collect()
-#     torch.cuda.empty_cache()
+    # (Optional) Clean up the cache.
+    del coordinate_grad, adv_suffix_tokens ; gc.collect()
+    torch.cuda.empty_cache()
     
